9cc43de9b97cd2e659fcf234429d79b61d4eb9ee
Eszter <szteran@gmail.com>
2024-01-23 12:09:22 +0100

    fetch-filter-map (countries)

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,32 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
-import MyComponent from './components/MyComponent';
 
 function App() {
-  //  variable, setter
-  let [data, setData] = useState(0)
+  const [ countries, setCountries ] = useState(null)
+  const [ searchString, setSearchString ] = useState("")
+  //console.log(countries)
+  //console.log(searchString)//ez mindig öres. settert kell használnom, hogy a searchString megváltozzon
 
-  console.log('component loaded')
+//ha valamit szeretnénk figyelni, hogyan viselkedik. Ne console.log-ba tegyük, hanem useEffectbe. Mert így mindig azt mutatja, hogy megváltozott valami. De csak akkor:
+
+  useEffect(() => console.log(countries), [countries])
+
+  useEffect(() => {
+    console.log("running fetch")//ez  csak egyszer fut le. 
+    fetch("https://restcountries.com/v3.1/all")
+      .then(res => res.json())
+      .then(data => setCountries(data)) //az5. sorban kimentettük a statet, ezért lejjebb is lesz majd data. Elérhető lesz. 
+  }, [])
 
   return (
     <div className="App">
-      data: {data}
+      <input type="text" onChange={event => setSearchString(event.target.value)}/>
 
-      <button onClick={() => {
-        setData((data) => data + 1)
-        }}>increase!</button>
-
-        <MyComponent number={data} setNumber={setData}/>
+      {countries !== null && countries
+        .filter(country => country.name.common.includes(searchString))
+        .map((country, index) => 
+          <div key={index}>{country.name.common}</div>
+      )}
     </div>
   );
 }
